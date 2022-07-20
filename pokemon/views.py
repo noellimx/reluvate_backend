e3576f5d9ea3b82842c5ab6671d02bb1ed5412c0
Noel Lim <noellimx@gmail.com>
2022-07-20 22:30:55 +0800

    add hello middleware

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,6 +1,11 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
 # Request handlers
 
 # Create your views here.
+
+
+def say_hello(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("Hello world from project[poke_project] app[pokemon]")
