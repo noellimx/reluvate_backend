7f71b338ba918f34a0025d159e8edacc35ac7910
Noel Lim <noellimx@gmail.com>
2022-07-21 07:14:04 +0800

    docs

---
+++ apis.md
@@ -0,0 +1,8 @@
+
+
+Params: -
+
+url -LX GET http://localhost:8000/mikecheck
+
+Returns:    Status 200
+            "copy copy app [pokemon]"
--- poke_project/urls.py
+++ poke_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include,re_path
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('mikecheck/', lambda _ :  HttpResponse("copy copy app [pokemon]")),
     path('admin/', admin.site.urls),
     path('pokemon/', include('pokemon.endpoints')),
     re_path(r'^auth/', include('djoser.urls.jwt')),
