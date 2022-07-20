0026029611ad2224519399bf34986233b3958cd8
Noel Lim <noellimx@gmail.com>
2022-07-21 07:45:26 +0800

    extend test for endpoint /mikecheck

--- integration_tests/test_endpoints.py
+++ integration_tests/test_endpoints.py
@@ -1,12 +1,20 @@
 from django.test import SimpleTestCase
 from pokemon.models import DummyModel
+from rest_framework import status
 
 from poke_project.urls import mikecheck_fn
 
 from django.urls import reverse, resolve
 
+from django.test import Client
+
 
 class TestEndpoints(SimpleTestCase):  # class name should prefix with `Test`
+
+    def setUp(self) -> None:
+        self.client = Client()
+
+
     def test_some_route(self):  # function name should prefix with `test_`
 
         url = reverse('application')
@@ -15,3 +23,7 @@ class TestEndpoints(SimpleTestCase):  # class name should prefix with `Test`
         endpoint = resolve(url)
 
         self.assertEquals(endpoint.func, mikecheck_fn)
+
+        response = self.client.get("/mikecheck/")
+
+        assert(response.status_code == status.HTTP_200_OK)
