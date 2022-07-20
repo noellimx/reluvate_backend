595bfa5afdd36f40dace805fdd38d6922e6a51a8
Noel Lim <noellimx@gmail.com>
2022-07-21 07:46:10 +0800

    fmt

--- integration_tests/test_endpoints.py
+++ integration_tests/test_endpoints.py
@@ -14,12 +14,9 @@ class TestEndpoints(SimpleTestCase):  # class name should prefix with `Test`
     def setUp(self) -> None:
         self.client = Client()
 
-
     def test_some_route(self):  # function name should prefix with `test_`
 
         url = reverse('application')
-
-
         endpoint = resolve(url)
 
         self.assertEquals(endpoint.func, mikecheck_fn)
